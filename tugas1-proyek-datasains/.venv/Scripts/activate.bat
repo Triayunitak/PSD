82e300f3751fc4de44c0af9e98800cdcadf1bc68
Triayunitak <triayunitakrismiyanto07@gmail.com>
2025-10-24 22:55:13 +0700

    add

--- tugas1-proyek-datasains/.venv/Scripts/activate.bat
+++ tugas1-proyek-datasains/.venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\masterprog\XAMPP\htdocs\PSD\tugas1-proyek-datasains\.venv
+set VIRTUAL_ENV=D:\masterprog\XAMPP2\htdocs\PSD\tugas1-proyek-datasains\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
